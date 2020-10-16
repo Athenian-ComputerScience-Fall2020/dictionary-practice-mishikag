0c66e6b1c42880a8b77656b6faf3aa1af1269961
mishikag <23mgupta@athenian.org>
2020-10-16 16:25:46 -0700

    dictionary practice and pytest finished

--- my_code.py
+++ my_code.py
@@ -7,14 +7,14 @@ def make_dict():
     keys = ['Ten', 'Twenty', 'Thirty']
     values = [10, 20, 30]
     '''
-    currency =          # complete this line
+    currency =   {'Ten': 10, 'Twenty': 20, 'Thirty': 30}       # complete this line
 
     return currency
 
 def add_element():
     foods = {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley'}
     # add a key value pair 'dairy': 'yogurt' to the following dictionary
-
+    foods['dairy'] = 'yogurt'
     # add code here
 
     return foods
@@ -22,7 +22,7 @@ def add_element():
 def remove_element():
     foods = {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley'}
     # remove 'veggie': 'carrot' from the dictionary
-
+    del foods['veggie']
 
     return foods
 
@@ -30,23 +30,19 @@ def merge_dict():
     # Merge these two dictionaries together so the contents are in numerical order:
     dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
     dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
-    # add code here
+    dict3 = ({'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Fourty': 40, 'Fifty': 50})
 
-    return     # return new dictionary
+    return dict3 # return new dictionary
 
 def access_key():
     # return the value of the key 'Twenty'
     currency = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 
-    val =             # add code to assign the desired value to 'val'
+    val = currency.get('Twenty')
     return val
-
-
-
 
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the function to test different sections
-    print(make_dict())
-
+    print(access_key())
